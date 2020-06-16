2b812ec57d5923090c5042c5db94bea9c5810f67
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-06-16 15:56:33 +0100

    Test for subtract passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -7,3 +7,8 @@ def add(first_num, second_num)
     return first_num + second_num
 end
 print add(1,2)
+
+def subtract(first_num, second_num)
+    return first_num - second_num
+end
+print subtract(10,5)
