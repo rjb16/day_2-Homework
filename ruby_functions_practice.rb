f83b07e2efa343bc70773964c805d6ac0dc688b2
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-06-16 16:13:38 +0100

    Test for join_string passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -22,3 +22,14 @@ def divide(first_num, second_num)
     return first_num / second_num
 end
 print (10,2)
+
+# def length_of_string(test_string)
+#     test_string = "A string of length 21"
+#     return test_string
+
+def join_string(string_1, string_2)
+    string_1 = "Mary had a little lamb, "
+    string_2 = "its fleece was white as snow"
+    return string_1 + string_2
+end 
+    print(join_string)
